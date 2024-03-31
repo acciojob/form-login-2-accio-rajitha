48e3441f66198d14e4a1b0908aeb905f6b232258
accio-rajitha <159473339+accio-rajitha@users.noreply.github.com>
2024-03-31 17:07:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+document.getElementById('myForm').addEventListener('submit', function(event) {
+            event.preventDefault();
+            const formData = new FormData(this);
+            let formInfo = '';
+            for (const [key, value] of formData.entries()) {
+                formInfo += `${key}: ${value} `;
+            }
+            alert(formInfo);
+        });
